d6e577d6b3b1ed7d6d37c9337779b31591dce52e
Yuuki Chan <yuuki-chan@protonmail.ch>
2020-07-13 14:47:09 +0900

    Updated to remove deprecated methods.

--- src/main/java/ladysnake/beem/Beem.java
+++ src/main/java/ladysnake/beem/Beem.java
@@ -1,33 +1,32 @@
 package ladysnake.beem;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Style;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class Beem implements ClientModInitializer {
-    private static final FabricKeyBinding TOGGLE_BEEM = FabricKeyBinding.Builder.create(
-            new Identifier("beem:toggle_beem"),
+    private static final KeyBinding TOGGLE_BEEM = new KeyBinding(
+            "beem:toggle_beem",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_B,
             "key.categories.misc"
-    ).build();
+    );
     static boolean beem = true;
 
     @Override
     public void onInitializeClient() {
         BlockEntityRendererRegistry.INSTANCE.register(BlockEntityType.BEEHIVE, BeeNestBlockEntityRenderer::new);
-        KeyBindingRegistry.INSTANCE.register(TOGGLE_BEEM);
-        ClientTickCallback.EVENT.register(e -> {
+        KeyBindingHelper.registerKeyBinding(TOGGLE_BEEM);
+        ClientTickEvents.START_CLIENT_TICK.register(e -> {
             while (TOGGLE_BEEM.wasPressed()) {
                 beem = !beem;
                 if (MinecraftClient.getInstance().player != null) {
